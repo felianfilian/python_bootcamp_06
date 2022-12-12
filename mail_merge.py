8d58d88cedffbd0df9635847823e3bb8e88fd2b9
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-12 11:29:56 +0100

    create automatic letters

--- mail_merge.py
+++ mail_merge.py
@@ -2,12 +2,13 @@
 
 def start():
     file = open("mail_names.txt", mode="r")
-    names = file.read().splitlines()
+    names = file.readlines()
     print(names)
     file_02 = open("./output/letter.txt", mode="r")
-    for name in range(len(names)):
-        letter = file_02.read()
-        letter.replace("[name]", names[name])
-        print(letter)
+    letter = file_02.read()
+    for name in names:
+        strip_name = name.strip()
+        new_letter = letter.replace("[name]", strip_name)
+        print(new_letter)
         # print(F"hello {names[name]}")
 
