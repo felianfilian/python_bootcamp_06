37fae6f4c01fd438e1a1bd2c2e22bec5c26125fc
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-12 23:34:38 +0100

    csv data with pandas

--- csv_test.py
+++ csv_test.py
@@ -1,8 +1,12 @@
-import csv
+# import csv
+import pandas
 
 def start():
-    ages = []
-    with open("csv_data.csv") as data_file:
-        data = csv.reader(data_file)
-        for row in data:
-            print(row[1])
+#     ages = []
+#     with open("csv_data.csv") as data_file:
+#         data = csv.reader(data_file)
+#         for row in data:
+#             print(row[1])
+    data = pandas.read_csv("csv_data.csv")
+    print(data)
+    print(data["name"])
