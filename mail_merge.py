89b1ee617a5659f8b72ab18fd4d7d148e4c0d2eb
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-12 11:21:39 +0100

    merge letter test

--- mail_merge.py
+++ mail_merge.py
@@ -4,4 +4,10 @@ def start():
     file = open("mail_names.txt", mode="r")
     names = file.read().splitlines()
     print(names)
+    file_02 = open("./output/letter.txt", mode="r")
+    for name in range(len(names)):
+        letter = file_02.read()
+        letter.replace("[name]", names[name])
+        print(letter)
+        # print(F"hello {names[name]}")
 
