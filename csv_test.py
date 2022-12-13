28bdcd1605fce74ed5105aeaf4e22995475f145a
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-13 23:22:29 +0100

    list conversion to dataframe

--- csv_test.py
+++ csv_test.py
@@ -7,7 +7,12 @@ def start():
 #         data = csv.reader(data_file)
 #         for row in data:
 #             print(row[1])
-    data = pandas.read_csv("csv_data.csv")
-    age_list = data["age"].to_list()
-    the_age = data[data.age == 33]
-    print(the_age.name)
+#     data = pandas.read_csv("csv_data.csv")
+#     age_list = data["age"].to_list()
+#     the_age = data[data.age == 33]
+#     print(the_age.name)
+    data_dic = {
+        "player": ["mario", "luigi", "bowser"],
+        "score": [120, 76, 158]
+    }
+    data = pandas.DataFrame(data_dic)
