6d3c37e8b54cadaefa38df38e278d3af7b96d9a4
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-13 23:46:16 +0100

    squirrel data init

--- csv_test.py
+++ csv_test.py
@@ -1,18 +1,4 @@
-# import csv
 import pandas
 
 def start():
-#     ages = []
-#     with open("csv_data.csv") as data_file:
-#         data = csv.reader(data_file)
-#         for row in data:
-#             print(row[1])
-#     data = pandas.read_csv("csv_data.csv")
-#     age_list = data["age"].to_list()
-#     the_age = data[data.age == 33]
-#     print(the_age.name)
-    data_dic = {
-        "player": ["mario", "luigi", "bowser"],
-        "score": [120, 76, 158]
-    }
-    data = pandas.DataFrame(data_dic)
+    data = pandas.read_csv("2")
