b00388374f3a1c2633f5f9ef29f43004101e8b98
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-14 10:50:31 +0100

    first visual screen

--- us_states_quiz.py
+++ us_states_quiz.py
@@ -1,4 +1,14 @@
+import turtle
 import pandas
 
 def start():
     print("Guess the US States")
+    screen = turtle.Screen()
+    screen.title("US States Quiz")
+    image = "./us_states/blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
+
+    
+
+    screen.exitonclick()
