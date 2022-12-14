7001dfa124873d61ee6f4780f98f8ea8d59341f6
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-14 10:15:08 +0100

    csv data count

--- csv_test.py
+++ csv_test.py
@@ -2,5 +2,7 @@ import pandas
 
 def start():
     data = pandas.read_csv("squirrel_data.csv")
-    grey_squirrels = data[data["Primary Fur Color"] == "Grey"]
+    grey_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+    black_squirrels = data[data["Primary Fur Color"] == "Black"]
     print(grey_squirrels)
+    print(black_squirrels)
