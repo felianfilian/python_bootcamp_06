6c78b5d55dd7ccb1701c11ad9105199961b67e35
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-14 20:11:10 +0100

    list comprehension test

---
+++ list_comp.py
@@ -0,0 +1,9 @@
+
+
+def start():
+    numbers = [1, 2, 3]
+
+    new_numbers = [n + 1 for n in numbers]
+    print(new_numbers)
+
+
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from us_states import us_states_quiz
+import list_comp
 
 # pong.start()
 # crossing.start()
 # mail_merge.start()
 # csv_test.start()
-us_states_quiz.start()
+# us_states_quiz.start()
+list_comp.start()
+
+
