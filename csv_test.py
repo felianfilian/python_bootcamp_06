9a3953ad3ddd2e0da5dc891d96c58f3e46fb3c3c
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-14 10:09:58 +0100

    get filtered data

--- csv_test.py
+++ csv_test.py
@@ -2,4 +2,5 @@ import pandas
 
 def start():
     data = pandas.read_csv("squirrel_data.csv")
-    data["Primary Fur Color"] == "Grey"
+    grey_squirrels = data[data["Primary Fur Color"] == "Grey"]
+    print(grey_squirrels)
