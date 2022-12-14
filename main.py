dae829c90e64847ae9deef9a2c3b5708a5d51136
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-14 20:49:43 +0100

    squared numbers

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from us_states import us_states_quiz
 import list_comp
+import square_numbers
 
 # pong.start()
 # crossing.start()
 # mail_merge.start()
 # csv_test.start()
 # us_states_quiz.start()
-list_comp.start()
+# list_comp.start()
+square_numbers.start()
 
 
---
+++ square_numbers.py
@@ -0,0 +1,7 @@
+
+def start():
+    numbers = [1,1,2,3,5,8,13,21,34,55]
+    squared_nums = [n * n for n in numbers]
+
+    print(squared_nums)
+
