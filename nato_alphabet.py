171bbc9d12e82bbe9779ba9c387524e605ccaf7c
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-16 00:51:37 +0100

    nato alphabet project completed

--- nato_alphabet.py
+++ nato_alphabet.py
@@ -3,7 +3,7 @@ import pandas
 
 def start():
     nato = pandas.read_csv("nato_phonetic_alphabet.csv")
-    nato_dic = nato.to_dict()
-    word = input("Your word: \n")
-    print(word)
-    print(nato_dic)
+    phon_dict = {row.letter: row.code for (index, row) in nato.iterrows()}
+    word = input("Your word: \n").upper()
+    nato_trans = {phon_dict[letter] for letter in word}
+    print(nato_trans)
