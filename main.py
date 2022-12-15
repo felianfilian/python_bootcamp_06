4e4806f669f632781bf87b840d54431acb2d6a7a
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-15 10:08:48 +0100

    dictionary test

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ import data_overlap
 # crossing.start()
 # mail_merge.start()
 # csv_test.start()
-us_states_quiz.start()
+# us_states_quiz.start()
 # list_comp.start()
 # square_numbers.start()
 # data_overlap.start()
 
+text = "mario is the fastest"
+txt_list = text.split()
+txt_dic = {name:len(name) for (name, count) in txt_list}
+print(txt_dic)
