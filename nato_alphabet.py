9bda383ecd6730f05128869d9b2c56421dc4df0c
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-21 12:56:31 +0100

    alphabet error solution

--- nato_alphabet.py
+++ nato_alphabet.py
@@ -5,9 +5,15 @@ def start():
     nato_trans = ""
     nato = pandas.read_csv("nato_phonetic_alphabet.csv")
     phon_dict = {row.letter: row.code for (index, row) in nato.iterrows()}
-    word = input("Your word: \n").upper()
-    try:
-        nato_trans = {phon_dict[letter] for letter in word}
-    except KeyError:
-        print("please use valid letters")
-    print(nato_trans)
+
+    def generate_phon():
+        word = input("Your word: \n").upper()
+        try:
+            nato_trans = {phon_dict[letter] for letter in word}
+        except KeyError:
+            print("please use valid letters")
+            generate_phon()
+        else:
+            print(nato_trans)
+
+    generate_phon()
