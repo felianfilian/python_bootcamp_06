1d9aef3a4dcd8d50d2bd9c710431efb725064613
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-21 12:53:28 +0100

    alphabet KeyError

--- nato_alphabet.py
+++ nato_alphabet.py
@@ -2,8 +2,12 @@ import pandas
 
 
 def start():
+    nato_trans = ""
     nato = pandas.read_csv("nato_phonetic_alphabet.csv")
     phon_dict = {row.letter: row.code for (index, row) in nato.iterrows()}
     word = input("Your word: \n").upper()
-    nato_trans = {phon_dict[letter] for letter in word}
+    try:
+        nato_trans = {phon_dict[letter] for letter in word}
+    except KeyError:
+        print("please use valid letters")
     print(nato_trans)
